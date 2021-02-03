62ac81d10fb962f2831cc7fa67257fe8ac6b01a8
beellz <belal20795@gmail.com>
2021-02-03 22:02:55 +0530

    Grep excersise done

---
+++ grep#3.sh
@@ -0,0 +1,9 @@
+#question link 
+#https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-3/problem
+
+#Answer Done in hacker Rank
+grep -viw "that"
+
+
+# Answer done locally
+grep -vwEi "that" ./sample.txt > wnsample.txt
