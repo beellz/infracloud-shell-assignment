9f39bbbc6d1b83ecd625c5b6df1df78612d0a19f
beellz <belal20795@gmail.com>
2021-02-05 12:34:46 +0530

    aded info in the solution

--- capital.sh
+++ capital.sh
@@ -1,5 +1,13 @@
+#Solution for the challenge of Remove the First Capital Letter From Each Element
+#Link https://www.hackerrank.com/challenges/bash-tutorials-remove-the-first-capital-letter-from-each-array-element
+
+
+
 arr=($(cat samplecountries))
 for i in "${arr[@]}"
 do
 echo -n .${i:1}" "
 done
+
+
+#samplecountries is for the sample data input.
